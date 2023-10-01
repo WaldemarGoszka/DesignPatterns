a189ffdee6c1a4b45a2b1f9be0d25ef5461fdeb5
Waldemar Goszka Surface Pro <goszka.waldemar@gmail.com>
2023-10-01 19:24:13 +0200

    builder

--- src/main/java/pl/zajavka/DesignPatterns/Builder/Main.java
+++ src/main/java/pl/zajavka/DesignPatterns/Builder/Main.java
@@ -1,4 +1,8 @@
 package pl.zajavka.DesignPatterns.Builder;
 
 public class Main {
+    public static void main(String[] args) {
+        CarCreator carCreator = new CarCreator();
+        carCreator.create();
+    }
 }
